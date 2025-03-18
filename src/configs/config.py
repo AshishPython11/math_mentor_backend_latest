6f8f71100777061f2f7d089d1f9453ed3f30c788
AshishPython11 <ashishpyton11@gmail.com>
2025-03-18 13:00:54 +0530

    change database name

--- src/configs/config.py
+++ src/configs/config.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# Define your database URL (replace with your actual DB details)
+
 DATABASE_URL = "postgresql://postgres:root@localhost:5432/mathmentor"
 
 
