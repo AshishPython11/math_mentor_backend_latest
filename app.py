930d933fae1112376b198dd5bac43e667b7a959e
AshishPython11 <ashishpyton11@gmail.com>
2025-03-21 16:32:53 +0530

    commented mcq router

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from logging.config import dictConfig
 from src.routers.auth import router as auth_router
 from src.routers.chat_ai import router as chat_router
 from src.routers.payment import router as payment_router
-from src.routers.ai_mcq import router as mcq_router
+# from src.routers.ai_mcq import router as mcq_router
 from src.configs.utilites import execute_sql_files
 from src.routers.profile import router as profile_router
 
@@ -21,7 +21,7 @@ def configure_routes(app: FastAPI) -> None:
     app.include_router(auth_router, prefix="/auth", tags=["Auth"])
     app.include_router(chat_router, prefix="/chatai", tags=["ChatAI"])
     app.include_router(payment_router, prefix="/payment", tags=["Payment"])
-    app.include_router(mcq_router, prefix="/mcq", tags=["mcq_routers"])
+    # app.include_router(mcq_router, prefix="/mcq", tags=["mcq_routers"])
     app.include_router(profile_router, prefix="/profile", tags=["profile_router"])
 
 
